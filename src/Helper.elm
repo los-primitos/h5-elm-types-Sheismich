bab55118e816ce0b5ba754cd7234e4830af65db7
Carlos Emanuel Muñiz <up230488@alumnos.upa.edu.mx>
2026-03-30 22:50:25 -0600

    Tarea 5 sheismich

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,64 @@
-module Helper exposing (..)
+module Helper exposing
+    ( AirplaneStatus(..)
+    , Grade(..)
+    , airplaneScheduleAction
+    , airportAction
+    , categoricalGrade
+    )
+
+-- TIPOS
+
+
+type Grade
+    = Approved
+    | Failed
+    | Pending
+
+
+type AirplaneStatus
+    = Cancelled
+    | Delayed
+    | OnTime
+    | Boarding
+
+
+
+-- FUNCIONES
+
+
+categoricalGrade : List Float -> List Grade
+categoricalGrade grades =
+    List.map gradeCategory grades
+
+
+gradeCategory : Float -> Grade
+gradeCategory g =
+    if g >= 7 then
+        Approved
+
+    else if g >= 0 then
+        Failed
+
+    else
+        Pending
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        Cancelled ->
+            "Pedir reembolso"
+
+        Delayed ->
+            "Esperar"
+
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction statuses =
+    List.map airplaneScheduleAction statuses
